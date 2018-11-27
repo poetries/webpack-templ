98c34afe2f60cdb9b9ab9bbb03fa1106eb3ca771
poetries <jingguanliuye@gmail.com>
2018-11-27 19:06:51 +0800

    react开发脚手架

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://blog.poetries.top"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            poetries
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
